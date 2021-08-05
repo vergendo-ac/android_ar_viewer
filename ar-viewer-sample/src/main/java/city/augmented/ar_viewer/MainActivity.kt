c0659d353c9bc928b06e1f0e5260be1d7b7f02e2
Itanik <yan.xhrome@gmail.com>
2021-08-05 21:07:53 +0300

    Add ARViewerFragment. Make full screen.

--- ar-viewer-sample/src/main/java/city/augmented/ar_viewer/MainActivity.kt
+++ ar-viewer-sample/src/main/java/city/augmented/ar_viewer/MainActivity.kt
@@ -2,10 +2,25 @@ package city.augmented.ar_viewer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import city.augmented.ar_viewer.databinding.ActivityMainBinding
+import city.augmented.ar_viewer_lib.ARViewerFragment
+import city.augmented.ar_viewer_lib.replaceFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewerFragment: ARViewerFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createViewerFragment(savedInstanceState)
+    }
+
+    private fun createViewerFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null)
+            supportFragmentManager.replaceFragment<ARViewerFragment>(R.id.viewer_fragment_container) {
+                viewerFragment = it
+            }
     }
 }
