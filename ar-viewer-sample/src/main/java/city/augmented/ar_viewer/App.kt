578579e8c6359c3606ee211a41b4c16f4405677f
Itanik <yan.xhrome@gmail.com>
2021-08-19 18:38:48 +0300

    Add timber logger lib.

---
+++ ar-viewer-sample/src/main/java/city/augmented/ar_viewer/App.kt
@@ -0,0 +1,11 @@
+package city.augmented.ar_viewer
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
