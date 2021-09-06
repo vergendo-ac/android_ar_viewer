23158c30363e3cafde3bbb40fabfead22833a35f
Itanik <yan.xhrome@gmail.com>
2021-09-06 20:24:37 +0300

    Configure take image job lifecycle.

--- ar-viewer-sample/src/main/java/city/augmented/ar_viewer/SimpleViewerFragment.kt
+++ ar-viewer-sample/src/main/java/city/augmented/ar_viewer/SimpleViewerFragment.kt
@@ -6,19 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.lifecycleScope
 import city.augmented.ar_viewer.databinding.FragmentSimpleViewerBinding
-import city.augmented.ar_viewer_lib.components.ArSceneState
 import city.augmented.ar_viewer_lib.presentation.ARViewerFragment
 import city.augmented.ar_viewer_lib.utils.replaceFragment
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import timber.log.Timber
 
 class SimpleViewerFragment : Fragment() {
     private val viewModel: SimpleViewerViewModel by viewModels()
     private lateinit var binding: FragmentSimpleViewerBinding
-    private lateinit var viewerFragment: ARViewerFragment
+    private var viewerFragment: ARViewerFragment? = null
+    private var localizingJob: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,25 +33,37 @@ class SimpleViewerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         if (savedInstanceState == null)
             parentFragmentManager.replaceFragment<ARViewerFragment>(R.id.viewer_fragment_container) { fragment ->
-                viewerFragment = fragment
                 fragment.setOnFragmentReadyListener {
-                    subscribeToSessionUpdates(it.sessionState)
+                    viewerFragment = fragment
+                    startTakingPictures()
                 }
             }
     }
 
-    private fun subscribeToSessionUpdates(sessionStateLiveData: LiveData<ArSceneState>) {
-        sessionStateLiveData.observe(viewLifecycleOwner) { state ->
-            Timber.d("ARViewerFragment state: $state")
-            when (state) {
-                ArSceneState.READY -> {
-                    lifecycleScope.launchWhenResumed {
-                        viewerFragment.imageDataFlow.collect { data ->
-                            Timber.d("Image data taken! Pose = ${data.syncPose}")
-                        }
-                    }
+    private fun startTakingPictures() = viewerFragment?.let { fragment ->
+        if (localizingJob == null) {
+            Timber.d("Starting take picture job")
+            localizingJob = lifecycleScope.launchWhenResumed {
+                fragment.imageDataFlow.collect { data ->
+                    Timber.i("Image data taken! Pose = ${data.syncPose}")
                 }
             }
         }
+    }
+
+    private fun stopTakingPictures() {
+        Timber.d("Stopping take picture job")
+        localizingJob?.cancel()
+        localizingJob = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startTakingPictures()
+    }
+
+    override fun onPause() {
+        stopTakingPictures()
+        super.onPause()
     }
 }
