1c0aa7992b3911517bd08974c373da2bd10dc6ac
Itanik <yan.xhrome@gmail.com>
2021-09-07 14:44:05 +0300

    Remove unused gravity provider.

--- ar-viewer-sample/src/main/java/city/augmented/ar_viewer/SimpleViewerViewModel.kt
+++ ar-viewer-sample/src/main/java/city/augmented/ar_viewer/SimpleViewerViewModel.kt
@@ -13,8 +13,6 @@ import city.augmented.api.model.GpsDto
 import city.augmented.api.model.ImageDescriptionDto
 import city.augmented.ar_viewer_lib.entity.ImageData
 import city.augmented.core.providers.GmsLocationProvider
-import city.augmented.core.providers.GravityProvider
-import city.augmented.core.providers.GravitySensorProvider
 import city.augmented.core.providers.LocationProvider
 import com.google.android.gms.location.FusedLocationProviderClient
 import kotlinx.coroutines.flow.first
@@ -23,7 +21,6 @@ import timber.log.Timber
 
 class SimpleViewerViewModel : ViewModel() {
     private lateinit var locationProvider: LocationProvider
-    private lateinit var gravityProvider: GravityProvider
     private val localizerApiClient: LocalizerApiClient = LocalizerApiClient(ACApiClient())
 
     private var _errorMessages: MutableLiveData<String> = MutableLiveData()
@@ -31,7 +28,6 @@ class SimpleViewerViewModel : ViewModel() {
 
     fun initProviders(context: Context) {
         locationProvider = GmsLocationProvider(FusedLocationProviderClient(context))
-        gravityProvider = GravitySensorProvider(context)
     }
 
     fun prepareLocalizationRequest(imageData: ImageData) = viewModelScope.launch {
